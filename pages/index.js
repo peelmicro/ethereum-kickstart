88b7352996664b16857c11605ecbcdb43b9f8360
juanp_perez@loyaltycrm.com <juanp_perez@loyaltycrm.com>
2018-03-31 19:38:26 +0200

    Setting Up Ethereum Network connection

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, {Component} from 'react'
+import factory from '../ethereum/factory'
 
-export default () => {
-  return <h1>Welcome to Home page!!!</h1>
+class CampaignIndex extends Component {
+  static async getInitialProps () {
+    const campaigns = await factory.methods.getDeployedCampaigns().call()
+    return { campaigns }
+  }
+  render() {
+    return <div>First Campaign: {this.props.campaigns[0]}</div>
+  }
 }
+
+export default CampaignIndex
