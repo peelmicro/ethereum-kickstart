a3c518c24a94a8785ea6816926df355d222b6e01
juanp_perez@loyaltycrm.com <juanp_perez@loyaltycrm.com>
2018-04-01 19:09:50 +0200

    Creating a Campaign Form

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import { Container } from 'semantic-ui-react'
+import Head from 'next/head'
+import Header from './Header'
 export default props => {
   return (
-    <div>
-      <h1>Im a header</h1>
+    <Container>
+      <Head>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>      
+      </Head>
+      <Header/>
       {props.children}
-      <h1>Im a footer</h1>
-    </div>
+    </Container>
   )
 }
