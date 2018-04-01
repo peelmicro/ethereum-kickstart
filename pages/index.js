7f666757bfcad74be09845fb1fd56c0058723d01
juanp_perez@loyaltycrm.com <juanp_perez@loyaltycrm.com>
2018-04-01 09:40:40 +0200

    Initial layout set up

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
 import React, {Component} from 'react'
 import factory from '../ethereum/factory'
+import { Card, Button } from 'semantic-ui-react'
+import Layout from '../components/Layout'
 
 class CampaignIndex extends Component {
   static async getInitialProps () {
     const campaigns = await factory.methods.getDeployedCampaigns().call()
     return { campaigns }
   }
+
+  renderCampaigns () {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      }
+    })
+
+    return <Card.Group items={items} />
+  }
   render() {
-    return <div>First Campaign: {this.props.campaigns[0]}</div>
+    return (
+      <Layout>
+        <div>
+          {/* This is a temporary solution */}
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+          <h3>Open Campaigns</h3>
+          {this.renderCampaigns()}
+          <Button 
+            content='Create Campaign' 
+            icon='add circle' 
+            primary
+          />
+        </div>
+      </Layout>
+    )
   }
 }
 
